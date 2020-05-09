a895a10435ddc4c509293174a76624c6d25ce2a5
Andre Gomes <andrem.gomes@outlook.com>
2020-05-09 16:22:24 -0300

    Separando arquivos back-end pra pasta api

--- api/src/models/database.js
+++ api/src/models/database.js
@@ -2,4 +2,3 @@ mysqlx.getSession('root:root@localhost:33060/blog')
     .then(session => {
        return session.getSchema('blog').getTable('user').select().execute(row => console.log(row))
     })
- 

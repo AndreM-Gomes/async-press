1920d0f921081f919f253aebc2672ea3af35609c
Luis Sousa <lv-ss@hotmail.com>
2020-05-09 22:15:08 -0300

    Home Page WIP

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/header'
+import Home from './pages/home'
+import './styles.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <Header/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;

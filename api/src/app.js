51c27659d345bed94c566f6aa66948c2d306859f
Andre Gomes <andrem.gomes@outlook.com>
2020-05-16 14:37:37 -0300

    Refatorando app.js e coreContainner

--- api/src/app.js
+++ api/src/app.js
@@ -1,1 +1,11 @@
-require('./injector')()
+const express = require('express')
+const app = express()
+
+const CoreContainner = require('./coreContainner')
+const core = new CoreContainner(app, 3000)
+
+core.start()
+  .catch(e => {
+    console.trace()
+    console.log(`Unhandled exception -> ${e}`)
+  })

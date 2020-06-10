6fafe9f6208b476c25367d8e2ebb34211d4f68f7
Luis Sousa <lv-ss@hotmail.com>
2020-06-09 21:24:03 -0300

    Adiconando Cors para permissão do Front

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import * as env from 'dotenv'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors( {origin: 'http://localhost:3001'})
   await app.listen(3000);
 }
 bootstrap();

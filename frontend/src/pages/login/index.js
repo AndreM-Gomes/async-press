f9fb3c081f9d85f565f7ce822e64575e8956b739
Luis Sousa <lv-ss@hotmail.com>
2020-06-09 22:01:45 -0300

    adicionado integração a api

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,18 +1,42 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {useState} from 'react'
+import {Link, useHistory} from 'react-router-dom'
 
 import './styles.css'
 import Header from '../../components/header'
+import api from '../../services/api'
 
 export default function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const history = useHistory()
+
+    async function handleLogin(e) {
+      e.preventDefault()
+
+      const data = {
+          email,
+          password,
+      };
+
+      try {
+        await api.post('auth/login', data)
+        history.push('/')
+      }
+      catch (err) {
+        alert('login ou senha incorretos')
+      }
+
+    }
+
     return(
       <div className="container">
         <Header/>
         <div className="card login">
-            <form>
-              <input type='email' placeholder='e-mail'/>
-              <input type='password' placeholder='Senha'/>
-              <button className='main' type='submit' value='Cadastrar'>Login</button>
+            <form onSubmit={handleLogin}>
+              <input type='email' placeholder='e-mail' onChange={e => setEmail(e.target.value)}/>
+              <input type='password' placeholder='Senha' onChange={e => setPassword(e.target.value)}/>
+              <button className='main' type='submit' value='Login'>Login</button>
             </form>
             <Link to='/register' className='link'>Me cadastar</Link>
         </div>

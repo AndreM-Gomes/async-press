844824409c1dbf9470d0af009ceaca9c0242373e
Luis Sousa <lv-ss@hotmail.com>
2020-06-12 01:21:07 -0300

    remoção da rota sidebar usado para testes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch } from 'react-router-dom'
+import {useAuth} from './services/auth'
 
 import Home from './pages/home'
 import Write from './pages/write'
@@ -7,19 +8,18 @@ import Login from './pages/login'
 import Register from './pages/register'
 import Profile from './pages/profile'
 import Dashboard from './pages/dashboard'
-import Sidebar from './components/sidebar'
 
 export default function Routes(){
+
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/sidebar' exact component={Sidebar}/>
-        <Route path='/' exact component={Home}/>
-        <Route path='/write' component={Write}/>
-        <Route path='/login' component={Login}/>
-        <Route path='/register' component={Register}/>
-        <Route path='/dashboard' component={Dashboard}/>
-        <Route path='/profile' component={Profile}/>
+          <Route path='/' exact component={Home}/>
+          <Route path='/write' component={Write}/>
+          <Route path='/login' component={Login}/>
+          <Route path='/register' component={Register}/>
+          <Route path='/dashboard' component={Dashboard}/>
+          <Route path='/profile' component={Profile}/>
       </Switch>
     </BrowserRouter>
   )

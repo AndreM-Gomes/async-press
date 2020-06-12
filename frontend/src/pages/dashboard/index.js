7f64c04a0f7da10686eca80baedcb17503712328
Luis Sousa <lv-ss@hotmail.com>
2020-06-12 02:01:01 -0300

    subustituido pseudoapi por api

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom'
 
 import './styles.css'
+import {useAuth} from '../../services/auth'
 
 
 export default function Dashboard(){
-  return(
-    <div>
-    </div>
-  )
+
+  const history = useHistory()
+  const loged = useAuth()
+  
+  if(loged){
+    return(
+      <div>
+      </div>
+    )
+  }else{
+    history.push('/register')
+    return null
+  }
 }

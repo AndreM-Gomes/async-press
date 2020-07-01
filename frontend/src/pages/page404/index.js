dedb2e661905975955169449e6048ff9f608c53d
Luis Sousa <lv-ss@hotmail.com>
2020-07-01 03:49:54 -0300

    Criado Pagina de Error 404

---
+++ frontend/src/pages/page404/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './styles.css'
+
+export default function Page404() {
+  return (
+  <div className='notFound'>
+    <h1>Erro 404</h1>
+    <h4>Hey, calma la, acho que voce pegou a Rota errada</h4>
+  </div>
+  )
+}

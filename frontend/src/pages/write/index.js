11370841f42850e5c67a77c1deea010104d0c62c
Luis Sousa <lv-ss@hotmail.com>
2020-07-01 03:42:22 -0300

    Adcionado Editor Markdown

--- frontend/src/pages/write/index.js
+++ frontend/src/pages/write/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import React , {useState}from 'react'
 import {useHistory} from 'react-router-dom'
+import ReactMde from 'react-mde'
+import * as Showdown from 'showdown'
+import "react-mde/lib/styles/css/react-mde-all.css";
 
 import './styles.css'
 import Header from '../../components/header'

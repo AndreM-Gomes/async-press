9ba776482a1e3f4aa5d64f64d576810170a25947
Luis Sousa <lv-ss@hotmail.com>
2020-07-01 04:02:55 -0300

    substituindo metodo de autenticação

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -7,27 +7,13 @@ import Header from '../../components/header'
 import api from '../../services/api'
 
 export default function Login() {
-    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const history = useHistory()
 
     async function handleLogin(e) {
       e.preventDefault()
-
-      const data = {
-          username,
-          password,
-      };
-
-      try {
-        const token = await api.post('auth/login', data).then(response =>  response.data)
-        login(token)
-        history.push('/')
-      }
-      catch (err) {
-        alert('login ou senha incorretos')
-      }
 
     }
 
@@ -36,7 +22,7 @@ export default function Login() {
         <Header/>
         <div className="card login">
             <form onSubmit={handleLogin}>
-              <input type='text' placeholder='username' onChange={e => setUsername(e.target.value)}/>
+              <input type='Email' placeholder='email' onChange={e => setEmail(e.target.value)}/>
               <input type='password' placeholder='Senha' onChange={e => setPassword(e.target.value)}/>
               <button className='main' type='submit' value='Login'>Login</button>
             </form>

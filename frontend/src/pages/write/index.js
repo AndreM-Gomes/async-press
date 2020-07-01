fe51a7b49b785b4b3b026b9f0ea9b814a8efa0d5
Luis Sousa <lv-ss@hotmail.com>
2020-07-01 03:41:36 -0300

    adcionado editor Markdown

--- frontend/src/pages/write/index.js
+++ frontend/src/pages/write/index.js
@@ -3,8 +3,17 @@ import {useHistory} from 'react-router-dom'
 
 import './styles.css'
 import Header from '../../components/header'
+const converter  = new Showdown.Converter({
+  tables: true,
+  simplifieldAutoLink: true,
+  striketrough: true,
+  tasklist: true,
+})
 
 export default function Write()  {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("");
+  const [selectedTab, setSelectedTab] = useState("write");
 
     const history = useHistory()
 
@@ -16,6 +25,26 @@ export default function Write()  {
           <Header/>
           <div className='write-page'>
             <imput></imput>
+  if(user) {
+    return(
+      <div className='container'>
+        <Header/>
+        <div className='write-page card'>
+          <form action="submit">
+            <input placeholder='title' onChange={setTitle}></input>
+            <div className='editor'>
+              <ReactMde
+                value={content}
+                onChange={setContent}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={markdown =>
+                  Promise.resolve(converter.makeHtml(markdown))
+              }/>
+            </div>
+          </form>
+          <div className="action">
+            <button className='main' onClick={handlePost}>Post</button>
           </div>
         </div>
       )

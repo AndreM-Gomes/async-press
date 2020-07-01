5ccbe38ee869aac09b7149dce65a8c0f051143a8
Luis Sousa <lv-ss@hotmail.com>
2020-07-01 03:59:36 -0300

    Adicionando Rota para Pagina Post

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -7,6 +7,7 @@ import Login from './pages/login'
 import Register from './pages/register'
 import Profile from './pages/profile'
 import Dashboard from './pages/dashboard'
+import Post from './pages/post'
 
 export default function Routes(){
 
@@ -16,6 +17,7 @@ export default function Routes(){
           <Route path='/' exact component={Home}/>
           <Route path='/write' component={Write}/>
           <Route path='/login' component={Login}/>
+          <Route path='/post' component={Post}/>
           <Route path='/register' component={Register}/>
           <Route path='/dashboard' component={Dashboard}/>
           <Route path='/profile' component={Profile}/>

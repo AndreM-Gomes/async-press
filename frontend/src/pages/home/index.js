aaa3ba94da5b0bf87f5482aac89c1d02cf7f141a
Luis Sousa <lv-ss@hotmail.com>
2020-07-03 03:32:35 -0300

    Separando components

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,35 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import Header from '../../components/header'
+import {Link} from 'react-router-dom'
 import api from '../../services/api'
-import firebase from 'firebase/app'
-import {FiMessageSquare, FiHeart, FiArchive, FiList, FiMic, FiVideo, FiTag} from 'react-icons/fi'
+import {FiMessageSquare, FiHeart} from 'react-icons/fi'
 
 import './styles.css';
-import Sidebar from '../../components/sidebar';
+import Header from '../../components/header'
+import SidebarTags from '../../components/sidebar-tags';
+import SidebarUtils from '../../components/sidebar-utils'
+import Stories from '../../components/stories'
 
 export default function Home() {
   const [buttonClass, setButtonClass] = useState('feed-button')
   const [postList, setPostList] = useState([])
   const [firstPost, setFirstPost] = useState([])
-  const [user, setUser] = useState(null)
 
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      setUser(user)
-      // ...
-    } else {
-      // User is signed out.
-      // ...
-    }
-  })
 
   useEffect(()=>{
       api.get('/post/latest').then(response => {setPostList(response.data)})
@@ -41,25 +25,9 @@ export default function Home() {
   return(
     <div className='container'>
         <Header/>
-        <Sidebar/><section className='leftList'>
-          <ul>
-            <li className='item user'>
-            {user && <>
-              <img src={user.photoURL} alt="User" className="imgMask"/>
-              <div>
-                <strong>{user.displayName}</strong>
-                <p>@incognitadev</p>
-              </div>
-            </>}
-            </li>   
-            <li className='item'><FiArchive size={18} className='icon'/>Reading list<span className="notify"></span></li>
-            <li className='item'><FiList size={18} className='icon'/>Listings<span className="notify"></span></li>
-            <li className='item'><FiMic size={18} className='icon'/>Podcasts<span className="notify"></span></li>
-            <li className='item'><FiVideo size={18} className='icon'/>Videos<span className="notify"></span></li>
-            <li className='item'><FiTag size={18} className='icon'/>Tags<span className="notify"></span></li>
-            <li className='item'>more...</li>
-          </ul>
-        </section>
+        <SidebarTags/>
+        <SidebarUtils/>
+        <Stories/>
         <nav className='feed-bar'>
           <h3>Posts</h3>
             <ul>
@@ -134,22 +102,6 @@ export default function Home() {
           ))}
         </div>
         <img src="https://dev-to-uploads.s3.amazonaws.com/i/got2x8dx1fu5xh00wsn8.png" alt="" className="g-image"/>
-        <section className="stories">
-          <h4>Stories</h4>
-           <ul>
-            <ul className="storie-item"><li >Lorem ipsum dolor sit amet consectetur adipisicing elit.</li></ul>
-            <ul className="storie-item"><li >Lorem ipsum dolor sit amet consectetur adipisicing elit. sjkdasdjhhdshfjdkshfsjdkfhjkdshf jfhsdkjfh sdjf </li></ul>
-            <ul className="storie-item"><li >Lorem ipsum dolor sit amet consectetur adipisicing elit.</li></ul>
-            <ul className="storie-item"><li >Lorem ipsum dolor sit amet consectetur adipisicing elit.</li></ul>
-            <ul className="storie-item"><li >Lorem ipsum dolor sit amet consectetur adipisicing elit.</li></ul>
-           </ul>
-           <div>
-           <button className='main'>Share Your Project</button>
-           </div>
-           <div>
-           <button className='secundary'>See All Posts</button>
-           </div>
-        </section>
       </div>
   )
 }

2de8a24c28cfe1342e495f33d138d8074d55a85a
Luis Sousa <lv-ss@hotmail.com>
2020-07-26 04:21:37 -0300

    adicionando dropdown menu

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,47 +1,59 @@
-import React , {useState}from 'react'
-import { Link } from 'react-router-dom'
-import {FiMessageCircle, FiBell} from  'react-icons/fi'
-import firebase from 'firebase/app'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FiMessageCircle, FiBell } from "react-icons/fi";
+import firebase from "firebase/app";
 
-
-import './styles.css'
+import "./styles.css";
 
 const Header = () => {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
 
-  firebase.auth().onAuthStateChanged(function(user) {
+  firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      // User is signed in.
-      setUser(user)
-      // ...
+      setUser(user);
     } else {
-      // User is signed out.
-      // ...
     }
   });
 
-  return  (
+  return (
     <div className="headerBar">
-      <div className='left-group'>
-        <Link to='/' className='logo'><span></span></Link>
-        <input className="search-box" placeholder='Search...'/>
+      <div className="left-group">
+        <Link to="/" className="logo">
+          <span></span>
+        </Link>
+        <input className="search-box" placeholder="Search..." />
       </div>
-      <div className='rigth-group'>
-        <Link to='/write' className='write'><button className='main btnHeader'>Write a post</button></Link>
+      <div className="right-group">
+        <Link to="/write" className="write">
+          <button className="main btnHeader">Write a post</button>
+        </Link>
         <div>
-          <span className='notify'></span>
-          <FiMessageCircle size={25} className='icon'/>
+          <span className="notify"></span>
+          <FiMessageCircle size={25} className="icon" />
         </div>
         <div>
-          <span className='notify'></span>
-          <FiBell size={25} className='icon'/>
+          <span className="notify"></span>
+          <FiBell size={25} className="icon" />
         </div>
-        {user &&
-        <img src={user.photoURL} alt="" className="imgMask"/>
-        }
+        {user ? (
+          <div className="dropdown">
+            <img src={user.photoURL} alt="" className="imgMask" />
+            <div className="dropdown-content">
+              <Link to="/profile">{user.displayName}</Link>
+            </div>
+          </div>
+        ) : (
+          <div className="dropdown">
+            <div className="dropdown-content">
+              <Link to="/login-by/">
+                <button className="main">Fazer Login</button>
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

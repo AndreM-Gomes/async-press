38b66bac0c346f09c0756d8f7ab84daf2cde9937
Luis Sousa <lv-ss@hotmail.com>
2020-07-26 04:22:50 -0300

    separando pagina em components

---
+++ frontend/src/components/post/SuggestionCard/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles.css";
+
+function SuggestionCard() {
+  return (
+    <div className="suggestion card">
+      <div className="header">
+        <h2>More from OIncognita</h2>
+      </div>
+    </div>
+  );
+}
+
+export default SuggestionCard;

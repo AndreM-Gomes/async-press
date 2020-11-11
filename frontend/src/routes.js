f8706c828c95df0afd3dc0b8f3f1d950e8d83f45
Luis Sousa <lv-ss@hotmail.com>
2020-11-11 01:17:43 -0300

    adicionando controle de rotas

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,29 +1,69 @@
-import React from 'react'
-import {BrowserRouter, Route, Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import Home from './pages/home'
-import Write from './pages/write'
-import Login from './pages/login'
-import Register from './pages/register'
-import Profile from './pages/profile'
-import Dashboard from './pages/dashboard'
-import Post from './pages/post'
-import Page404 from './pages/page404'
+import { isAuthenticated } from "./services/auth";
 
-export default function Routes(){
+import Home from "./pages/home";
+import Write from "./pages/write";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import LoginProvider from "./pages/LoginProviders";
+import Profile from "./pages/profile";
+import Dashboard from "./pages/dashboard";
+import Post from "./pages/post";
+import Page404 from "./pages/page404";
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/registerProvider/register",
+            state: { from: props.location },
+          }}
+        />
+      )
+    }
+  />
+);
+
+const SignRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Redirect
+          to={{
+            pathname: "/registerProvider/register",
+            state: { from: props.location },
+          }}
+        />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
+
+export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route path='/write' component={Write}/>
-          <Route path='/login' component={Login}/>
-          <Route path='/post' component={Post}/>
-          <Route path='/register' component={Register}/>
-          <Route path='/dashboard' component={Dashboard}/>
-          <Route path='/profile' component={Profile}/>
-          <Route path='/*'component={Page404}/>
+        <Route path="/" exact component={Home} />
+        <Route path="/post/:id" component={Post} />
+        <SignRoute path="/login-by/login" component={Login} type='Login'/>
+        <SignRoute path="/register-by/register" component={Register} type='Register'/>
+        <SignRoute path="/login-by" component={LoginProvider} />
+        <SignRoute path="/register-by" component={LoginProvider} />
+        <PrivateRoute path="/write" component={Write} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/profile" component={Profile} />
+        <Route path="/*" component={Page404} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }

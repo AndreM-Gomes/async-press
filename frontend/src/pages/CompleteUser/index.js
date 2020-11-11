b443688191cf5e5df612835b3620294269b52003
Luis Sousa <lv-ss@hotmail.com>
2020-11-11 05:08:32 -0300

    criando pagina de completar cadastro

---
+++ frontend/src/pages/CompleteUser/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./styles";
+import Header from "../../components/header";
+
+function CompleteUser() {
+  return (
+    <div className="Container">
+      <Header />
+    </div>
+  );
+}
+
+export default CompleteUser;

cc024fb9c94da33890e57ba83ce57f0e8636ab1e
Luis Sousa <lv-ss@hotmail.com>
2020-11-11 01:17:17 -0300

    adicionando verificador de autenticação

--- frontend/src/services/auth/index.js
+++ frontend/src/services/auth/index.js
@@ -1,5 +1,4 @@
-import firebase from 'firebase/app'
-
+import firebase from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAaBPn-M_5S70oKQ0JRNrd5rFYT1DFEH1A",
@@ -9,12 +8,13 @@ const firebaseConfig = {
   storageBucket: "async-press.appspot.com",
   messagingSenderId: "562877451451",
   appId: "1:562877451451:web:8f2d96dd96d7f31ce16d9e",
-  measurementId: "G-KZQRP0EN9J"
+  measurementId: "G-KZQRP0EN9J",
 };
 
-  export const fbInit = () => {
-    if(!firebase.apps.length){
-      firebase.initializeApp(firebaseConfig);
-    }
+export const fbInit = () => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
   }
+};
 
+export const isAuthenticated = () => firebase.auth().currentUser;
